1aa3c43fcada6bc163246f18e5bf0d45ed17d965
Libby Moore <50882057+libbymoore12@users.noreply.github.com>
2019-05-21 11:09:39 -0600

    Update starterarduino.ino

--- starterarduino.ino
+++ starterarduino.ino
@@ -1,1 +1,16 @@
 //just kidding this is the actual one
+
+void setup() 
+{
+ Serial.begin(9600);
+ pinMode(12,INPUT);
+ 
+}
+
+void loop() 
+{
+  int onOff=digitalRead(12);
+  Serial.println(onOff);
+delay(50);
+
+}
