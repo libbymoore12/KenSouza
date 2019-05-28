7f4104aeaf364cf84fb25a3e1d5fd7aaad6038e7
Libby Moore <50882057+libbymoore12@users.noreply.github.com>
2019-05-28 11:47:49 -0600

    Update thermistor.ino

--- thermistor.ino
+++ thermistor.ino
@@ -1,9 +1,18 @@
+//works for reading in basic temperature data and printing to the serial monitor
+
+int value=0;
+
 void setup() // set up the electronics
 {
-  // code
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+  
 }
 
 void loop() // continous check to use by electronics
 { 
-  // code
+  value = analogRead(A0);
+  Serial.println(value);
+  
+  delay(10);
 }
