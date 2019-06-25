5cf0f89d209b43c63a81de9dbfd92d38db7afb31
CorinthianCore <50882090+CorinthianCore@users.noreply.github.com>
2019-06-25 12:44:22 -0600

    Update ARES_DemoSat_Code.ino

--- ARES_DemoSat_Code.ino
+++ ARES_DemoSat_Code.ino
@@ -1,4 +1,3 @@
-
 /*
 Contributors: Corey LePine, Libby Moore, Jake S. (6/25/19)
  
@@ -11,7 +10,12 @@ Updated 01/04/2017 for OpenLog
    
 */
 
+/// Libraries
+#include <Wire.h>
+#include "RTClib.h"
 
+
+/// Definitions
    
    /*
    Ammonia Sensor Definitions
@@ -94,6 +98,14 @@ Updated 01/04/2017 for OpenLog
   /*
    Clock Definitions
   */
+     #if defined(ARDUINO_ARCH_SAMD)
+     // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+        #define Serial SerialUSB
+     #endif
+
+     RTC_DS1307 rtc;
+
+     char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
   /*
    SD Card Definitions
@@ -107,7 +119,7 @@ void setup()
 {
   Serial.begin(9600);
  
-  Serial.println("Header for sensors")
+  Serial.println("Header for sensors");
 
   /*
    Switch Setup
@@ -116,6 +128,25 @@ void setup()
   /*
    Clock Setup
   */
+   #ifndef ESP8266
+     while (!Serial); // for Leonardo/Micro/Zero
+   #endif
+
+     if (! rtc.begin()) 
+     {
+       Serial.println("Couldn't find RTC");
+       while (1);
+     }
+
+     if (! rtc.isrunning()) 
+     {
+       Serial.println("RTC is NOT running!");
+       // following line sets the RTC to the date & time this sketch was compiled
+       rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+       // This line sets the RTC with an explicit date & time, for example to set
+       // January 21, 2014 at 3am you would call:
+       // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+     }
 
   /*
    SD Card Setup
@@ -124,7 +155,6 @@ void setup()
   /*
    Battery Gauge Setup
   */
-
 }
 
 void loop() 
@@ -261,6 +291,49 @@ void loop()
   /*
    Clock Loop
   */
+ 
+    DateTime now = rtc.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+    
+    // calculate a date which is 7 days and 30 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+    
+    Serial.print(" now + 7d + 30s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+    
+    Serial.println();
+    delay(3000);
 
   /*
    SD Card Loop
