6fc922e7dd61e66be50ce61b8a7957e9f30675d6
CorinthianCore <50882090+CorinthianCore@users.noreply.github.com>
2019-06-27 09:07:19 -0600

    Update ARES_DemoSat_Code.ino

--- ARES_DemoSat_Code.ino
+++ ARES_DemoSat_Code.ino
@@ -342,23 +342,6 @@ void loop()
     Serial.print(now.unixtime() / 86400L);
     Serial.println("d");
     
-    // calculate a date which is 7 days and 30 seconds into the future
-    DateTime future (now + TimeSpan(7,12,30,6));
-    
-    Serial.print(" now + 7d + 30s: ");
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(' ');
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-    Serial.println();
-    
     Serial.println();
     delay(3000);
 
